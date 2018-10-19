7d6f25e809ae4dea974d6306977354825b5fac5d
apollo78124 <98apollo@naver.com>
2018-10-19 15:11:05 -0700

    h

--- app/src/main/java/ca/bcit/android_project/MainActivity.java
+++ app/src/main/java/ca/bcit/android_project/MainActivity.java
@@ -1,13 +1,25 @@
 package ca.bcit.android_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void map(View v) {
+        Intent intent1 = new Intent(this, mapActivity.class);
+        startActivity(intent1);
+    }
+
+    public void list(View v) {
+        Intent intent1 = new Intent(this, listActivity.class);
+        startActivity(intent1);
     }
 }
