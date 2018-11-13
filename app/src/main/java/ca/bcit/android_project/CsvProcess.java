e4e006ec10f1053c5adc87bf352209a832df5704
apollo78124 <98apollo@naver.com>
2018-11-13 14:00:07 -0800

    Csv Processing done.

--- app/src/main/java/ca/bcit/android_project/CsvProcess.java
+++ app/src/main/java/ca/bcit/android_project/CsvProcess.java
@@ -4,17 +4,43 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class CsvProcess {
     public double userLat;
     public double userLong;
-    String csvFile = "./res/values/Property_Crimes.csv";
+    String csvFileSuggestedAddress = "./res/values/Property_Crimes.csv";
 
     public CsvProcess() {
+
+    }
+
+    public ArrayList<String[]> csvProcessing(String fileAddress) {
+        ArrayList<String[]> stringlist = new ArrayList<String[]>();
+        String csvFile = fileAddress;
         BufferedReader br = null;
         String line = "";
         String cvsSplitBy = ",";
-
+        /**
+         * Return ArrayList of String array.
+         * use country[number] to refer to certain attribute inside the arraylist.
+         * Number reference list
+         * [0]X,
+         * [1]Y,
+         * [2]OBJECTID,
+         * [3]FileNumber,
+         * [4]OccuranceYear,
+         * [5]ReportedDate,
+         * [6]ReportedTime,
+         * [7]ReportedWeekday,
+         * [8]Offense,
+         * [9]OffenseCategory,
+         * [10]HouseNumber,
+         * [11]StreetName,
+         * [12]City,
+         * [13]ReportedDateText,
+         * [14]ReportedTimeText
+         */
         try {
 
             br = new BufferedReader(new FileReader(csvFile));
@@ -22,7 +48,7 @@ public class CsvProcess {
 
                 // use comma as separator
                 String[] country = line.split(cvsSplitBy);
-
+                stringlist.add(country);
             }
 
         } catch (FileNotFoundException e) {
@@ -38,9 +64,8 @@ public class CsvProcess {
                 }
             }
         }
+        return stringlist;
     }
-
-
 
 }
 
