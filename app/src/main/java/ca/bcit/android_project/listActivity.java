2fe2ebdf82bfdb6bfdcb0a6ca5d02d4f66cbfcdd
mikah <anhminhhoang13@gmail.com>
2018-11-22 11:43:45 -0800

    add map button

--- app/src/main/java/ca/bcit/android_project/listActivity.java
+++ app/src/main/java/ca/bcit/android_project/listActivity.java
@@ -19,12 +19,9 @@ import ca.bcit.android_project.model.Crime;
 import ca.bcit.android_project.service.CsvProcess;
 
 public class listActivity extends AppCompatActivity {
-    private Toolbar mTopToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        mTopToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(mTopToolbar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
         final ArrayList<Crime> crimesList =  CsvProcess.convertCsvToListOfCrimes(this);
@@ -50,34 +47,8 @@ public class listActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.list_menu, menu);
 
-        // Associate searchable configuration with the SearchView
-        SearchManager searchManager =
-                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView =
-                (SearchView) menu.findItem(R.id.search).getActionView();
-        searchView.setSearchableInfo(
-                searchManager.getSearchableInfo(getComponentName()));
 
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.add:
-//                Intent i = new Intent(this, PersonFormActivity.class);
-//                i.putExtra("edit", false); // not necessary; for readability
-//                startActivity(i);
-                return true;
-
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
 
 }
