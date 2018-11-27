8ca500acd1cb2cc214e5588fc593ea388357d864
mikah <anhminhhoang13@gmail.com>
2018-11-27 00:52:46 -0800

    Fix UI for main activity

--- app/src/main/java/ca/bcit/android_project/CrimeActivity.java
+++ app/src/main/java/ca/bcit/android_project/CrimeActivity.java
@@ -3,11 +3,7 @@ package ca.bcit.android_project;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
-
-import java.util.ArrayList;
 
 public class CrimeActivity extends AppCompatActivity {
 
