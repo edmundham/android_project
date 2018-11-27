f412332eae8b3ec94c4ca8be808696d027d6adef
mikah <anhminhhoang13@gmail.com>
2018-11-27 12:53:25 -0800

    fix UI for crime layout

--- app/src/main/java/ca/bcit/android_project/CrimeActivity.java
+++ app/src/main/java/ca/bcit/android_project/CrimeActivity.java
@@ -15,41 +15,31 @@ public class CrimeActivity extends AppCompatActivity {
         String data = intent.getStringExtra("data");
         String[] crimeObject = data.split(",");
 
-        String objectId = crimeObject[2];
         String occuranceYear = crimeObject[4];
         String reportedDate = crimeObject[5];
         String offense = crimeObject[8];
-        String offenseCategory = crimeObject[9];
         String houseNumber = crimeObject[10].replace('X', '0');
         String streetName = crimeObject[11];
         String city = crimeObject[12];
+        if(!houseNumber.trim().equals("")){
+            houseNumber += ", ";
+        }
+        String address = houseNumber + streetName + ", " + city;
         String reportedDateText = crimeObject[13];
-        String lat = crimeObject[15];
-        String lon = crimeObject[16];
 
-        TextView objectIdtv = findViewById(R.id.textView16);
-        TextView occuranceYeartv = findViewById(R.id.textView15);
+
         TextView reportedDatetv = findViewById(R.id.textView14);
         TextView offensetv = findViewById(R.id.textView13);
-        TextView offenseCategorytv = findViewById(R.id.textView12);
-        TextView houseNumbertv = findViewById(R.id.textView11);
-        TextView streetNametv = findViewById(R.id.textView10);
-        TextView citytv = findViewById(R.id.textView9);
+        TextView addresstv = findViewById(R.id.textView11);
         TextView reportedDateTexttv = findViewById(R.id.textView8);
-        TextView lattv = findViewById(R.id.textView7);
-        TextView lontv = findViewById(R.id.textView6);
 
-        objectIdtv.setText(objectId);
-        occuranceYeartv.setText(occuranceYear);
+
+
         reportedDatetv.setText(reportedDate);
         offensetv.setText(offense);
-        offenseCategorytv.setText(offenseCategory);
-        houseNumbertv.setText(houseNumber);
-        streetNametv.setText(streetName);
-        citytv.setText(city);
+        addresstv.setText(address);
         reportedDateTexttv.setText(reportedDateText);
-        lattv.setText(lat);
-        lontv.setText(lon);
+
 
 
 
